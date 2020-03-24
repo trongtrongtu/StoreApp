12e6347171521fc00b35e02cf8e4df8fc5504790
trongtrongtu <trongtrongtu169@gmail.com>
2020-03-24 19:13:09 +0700

    Tab Navigation

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Category from './screens/Category';
 import Categories from './screens/Categories';
+import Cart from './screens/Cart';
+import Orders from './screens/Orders';
+import Settings from './screens/Settings';
 
-const AppNavigator = createStackNavigator({
-    Categories: {
-        screen: Categories
-    },
-    Category: {
-        screen: Category
-    }
+const CategoryStack = createStackNavigator({
+    Categories,
+    Category
 });
+CategoryStack.navigationOptions = {
+    tabBarLabel: 'Home'
+}
+const CartStack = createStackNavigator({
+    Cart
+});
+CartStack.navigationOptions = {
+    tabBarLabel: 'Cart'
+}
+const OrederStack = createStackNavigator({ Orders });
+OrederStack.navigationOptions = {
+    tabBarLabel: 'Oreders'
+}
+const SettingStack = createStackNavigator({ Settings })
+SettingStack.navigationOptions = {
+    tabBarLabel: 'Settings'
+}
 
+const AppNavigator = createBottomTabNavigator({
+    CategoryStack,
+    CartStack,
+    OrederStack,
+    SettingStack
+})
 export default AppNavigator;
